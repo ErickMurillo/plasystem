0dcdb0f398bec600ae3bc79968c3f94b4b8b7062
Carlos Rocha <crocha09.09@gmail.com>
2017-03-20 10:20:36 -0600

    agregando plan anual

--- subsectores/models.py
+++ subsectores/models.py
@@ -93,4 +93,82 @@ class UmbrelaDesempeno(models.Model):
         verbose_name = 'Umbrela de desempeño'
         verbose_name_plural = 'Umbrelas de desempeños'
 
+#--------- registro de planes anuales --------
+
+@python_2_unicode_compatible
+class TipoItem(models.Model):
+    nombre = models.CharField(max_length=250)
+
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = 'Tipo item'
+        verbose_name_plural = 'Tipos items'
+
+@python_2_unicode_compatible
+class Actividades(models.Model):
+    nombre = models.CharField(max_length=250)
+
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = 'Actividad'
+        verbose_name_plural = 'Actividades'
+
+
+@python_2_unicode_compatible
+class CategoriaGastos(models.Model):
+    nombre = models.CharField(max_length=250)
+
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = 'Categoria de gasto'
+        verbose_name_plural = 'Categorias de gatos'
+
+
+@python_2_unicode_compatible
+class RegistroPlanAnual(models.Model):
+    item = models.CharField(max_length=250)
+    tipo = models.ForeignKey(TipoItem)
+    indicador = models.ForeignKey(Indicadores)
+    actividad = models.ForeignKey(Actividades)
+    indicador2 = models.CharField(max_length=250)
+    categoria = models.ForeignKey(CategoriaGastos)
+
+    def __str__(self):
+        return self.item
+
+    class Meta:
+        verbose_name = 'Registro plan anual'
+        verbose_name_plural = 'Registros planes anuales'
+
+
+CHOICES_MESES = ((1,'Enero'),(2,'Febrero'),
+                 (3,'Marzo'),(4,'Abril'),
+                 (5,'Mayo'),(6,'Junio'),
+                 (7,'Julio'),(8,'Agosto'),
+                 (9,'Septiembre'),(10,'Octubre'),
+                 (11,'Noviembre'),(12,'Diciembre'),)
+
+@python_2_unicode_compatible
+class RegistroMeses(models.Model):
+    registro_anual = models.ForeignKey(RegistroPlanAnual)
+    mes = models.IntegerField(choices=CHOICES_MESES)
+    meta = models.FloatField()
+    presupuesto = models.FloatField()
+    
+
+    def __str__(self):
+        return self.registro_anual.item
+
+    class Meta:
+        verbose_name = 'Registro de mes'
+        verbose_name_plural = 'Registro de meses'
 
