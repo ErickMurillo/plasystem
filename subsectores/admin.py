ac9bc45a298cb59df80f3e3630f710b13f57b639
Carlos Rocha <crocha09.09@gmail.com>
2017-03-21 10:12:55 -0600

    admin subsectore

--- subsectores/admin.py
+++ subsectores/admin.py
@@ -9,8 +9,16 @@ admin.site.register(Indicadores)
 admin.site.register(Aspectos)
 admin.site.register(UmbrelaDesempeno)
 #register for plan anual
+
+class RegistroMesesInline(admin.TabularInline):
+	model = RegistroMeses
+	extra = 1
+
+class RegistroPlanAnualAdmin(admin.ModelAdmin):
+	inlines = [RegistroMesesInline]
+
 admin.site.register(TipoItem)
 admin.site.register(Actividades)
 admin.site.register(CategoriaGastos)
-admin.site.register(RegistroPlanAnual)
+admin.site.register(RegistroPlanAnual, RegistroPlanAnualAdmin)
 admin.site.register(RegistroMeses)
