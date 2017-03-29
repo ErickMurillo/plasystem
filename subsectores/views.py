c87fee1192fab48e76e81b1a3bd92e00b11f9f71
Carlos Rocha <crocha09.09@gmail.com>
2017-03-29 17:53:07 -0600

    fix error models

--- subsectores/views.py
+++ subsectores/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import RegistroPlanAnual, CHOICES_MESES
+from .models import RegistroPlanAnual, CHOICES_MESES, DatosGenerales
 # Create your views here.
 
 
@@ -10,5 +10,5 @@ def list_informe(request, template='admin/lista_informe_plan_anual.html'):
 def ver_informe(request, template='admin/ver_informe.html', pk=None):
 	proyecto = get_object_or_404(DatosGenerales, id=pk)
 	meses = CHOICES_MESES
-	informe = RegistroPlanAnual.objects.filter(proyecto__componente__id=pk)
+	informe = RegistroPlanAnual.objects.filter(proyecto__id=pk)
 	return render(request, template, locals())
