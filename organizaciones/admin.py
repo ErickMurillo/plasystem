accced58cb65d1d0fdd9ff4ebfc914da615d60bc
Carlos Rocha <crocha09.09@gmail.com>
2017-05-11 12:02:47 -0600

    agregando mas modelos

--- organizaciones/admin.py
+++ organizaciones/admin.py
@@ -1,6 +1,42 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 from .models import *
 
+class OrganizacionAdmin(admin.ModelAdmin):
+	#fields = (('nombre', 'tipo'), 'direccion')
+	# fields = (
+	# 	('nombre', 'tipo', 'direccion'), 
+	# 	('pais','departamento','municipio'),
+	# 	('oficina_numero','oficina_correo','sitio_web'),
+	# 	('red_social1','red_social2','red_social3','red_social4'),
+	# 	('persona1_contacto','persona1_cargo','persona1_telefono','persona1_correo'),
+	# 	('persona2_contacto','persona2_cargo','persona2_telefono','persona2_correo'),
+	# 	)
+
+	fieldsets = (
+        (None, {
+            'fields': (('nombre', 'tipo', 'direccion'),('pais',
+            		   'departamento','municipio'),('oficina_numero',
+            		   'oficina_correo'),'sitio_web')
+        }),
+        ('Redes Sociales', {
+            'fields': (('red_social1','red_social2'),
+            			('red_social3','red_social4')),
+        }),
+        ('Persona de contacto 1', {
+            'fields': (('persona1_contacto','persona1_cargo'),
+            		   ('persona1_telefono','persona1_correo')),
+        }),
+        ('Persona de contacto 2', {
+            'fields': (('persona2_contacto','persona2_cargo'),
+            		  ('persona2_telefono','persona2_correo')),
+        }),
+        ('Situación legal y organizativa de la organización', {
+            'fields': (('personeria','en_operaciones','incorporacion'),
+            		  ('licencia','tributaria'),'vision','mision','apoyo'),
+        }),
+    )
+
 # Register your models here.
-admin.site.register(Organizacion)
+admin.site.register(Organizacion, OrganizacionAdmin)
 admin.site.register(TipoOrganizacion)
--- organizaciones/models.py
+++ organizaciones/models.py
@@ -17,33 +17,52 @@ class TipoOrganizacion(models.Model):
         verbose_name = 'Tipo de Organización'
         verbose_name_plural = 'Tipos de Organizaciones'
 
+CHOICE_PERSONERIA = (
+        (1, 'Si'),
+        (2, 'No'),
+        (3, 'En proceso')
+    )
+
+CHOICE_SI_NO = (
+        (1, 'Si'),
+        (2, 'No'),
+    )
+
 @python_2_unicode_compatible
 class Organizacion(models.Model):
     nombre = models.CharField(max_length = 200)
     tipo = models.ForeignKey(TipoOrganizacion)
     direccion = models.CharField(max_length=300, null=True, blank=True)
-    pais = models.ForeignKey(Pais)
-    departamento = models.ForeignKey(Departamento)
-    municipio = models.ForeignKey(Municipio)
+    pais = models.ForeignKey(Pais, null=True, blank=True)
+    departamento = models.ForeignKey(Departamento, null=True, blank=True)
+    municipio = models.ForeignKey(Municipio, null=True, blank=True)
     oficina_numero = models.CharField(max_length=12, null=True, blank=True)
     oficina_correo = models.EmailField(max_length=254, null=True, blank=True)
     sitio_web = models.URLField(null=True, blank=True)
     #redes sociales
-    red_social1 = models.CharField(max_length=250, null=True, blank=True)
-    red_social2 = models.CharField(max_length=250, null=True, blank=True)
-    red_social3 = models.CharField(max_length=250, null=True, blank=True)
-    red_social4 = models.CharField(max_length=250, null=True, blank=True)
+    red_social1 = models.CharField('Red Social 1', max_length=250, null=True, blank=True)
+    red_social2 = models.CharField('Red Social 2', max_length=250, null=True, blank=True)
+    red_social3 = models.CharField('Red Social 3', max_length=250, null=True, blank=True)
+    red_social4 = models.CharField('Red Social 4', max_length=250, null=True, blank=True)
     #persona 1 de contacto
-    persona1_contacto = models.CharField(max_length=300, null=True, blank=True)
-    persona1_cargo = models.CharField(max_length=250, null=True, blank=True)
-    persona1_telefono = models.CharField(max_length=12, null=True, blank=True)
-    persona1_correo = models.EmailField(max_length=254, null=True, blank=True)
+    persona1_contacto = models.CharField('Nombre', max_length=300, null=True, blank=True)
+    persona1_cargo = models.CharField('Cargo', max_length=250, null=True, blank=True)
+    persona1_telefono = models.CharField('Telefono', max_length=12, null=True, blank=True)
+    persona1_correo = models.EmailField('Correo', max_length=254, null=True, blank=True)
     #persona 2 de contacto
-    persona2_contacto = models.CharField(max_length=300, null=True, blank=True)
-    persona2_cargo = models.CharField(max_length=250, null=True, blank=True)
-    persona2_telefono = models.CharField(max_length=12, null=True, blank=True)
-    persona2_correo = models.EmailField(max_length=254, null=True, blank=True)
-
+    persona2_contacto = models.CharField('Nombre', max_length=300, null=True, blank=True)
+    persona2_cargo = models.CharField('Cargo', max_length=250, null=True, blank=True)
+    persona2_telefono = models.CharField('Telefono', max_length=12, null=True, blank=True)
+    persona2_correo = models.EmailField('Correo', max_length=254, null=True, blank=True)
+    #situacion legal
+    personeria = models.IntegerField('Cuenta con personería Jurídica', choices=CHOICE_PERSONERIA, null=True, blank=True)
+    en_operaciones = models.IntegerField(choices=CHOICE_SI_NO, null=True, blank=True)
+    incorporacion = models.CharField('Año de incorporación', max_length=250, null=True, blank=True)
+    licencia = models.CharField('No. de licencia empresarial', max_length=250, null=True, blank=True)
+    tributaria = models.CharField('No. de identificación tributaria', max_length=250, null=True, blank=True)
+    vision = models.TextField(null=True, blank=True)
+    mision = models.TextField(null=True, blank=True)
+    apoyo = models.IntegerField(choices=CHOICE_SI_NO, null=True, blank=True)
 
 
     def __str__(self):
@@ -52,3 +71,30 @@ class Organizacion(models.Model):
     class Meta:
         verbose_name = 'Organización'
         verbose_name_plural = 'Organizaciones'
+
+class Intituciones(models.Model):
+    nombre = models.CharField(max_length=250)
+
+    def __unicode__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name_plural = 'Instituciones'
+
+class Areas(models.Model):
+    nombre = models.CharField(max_length=250)
+
+    def __unicode__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name_plural = 'Areas de apoyo'
+
+class ApoyoDonante(models.Model):
+    institucion = models.ForeignKey(Intituciones)
+    areas = models.ManyToManyField(Areas)
+
+    organizacion = models.ForeignKey(Organizacion)
+
+    def __unicode__(self):
+        return self.institucion.nombre
