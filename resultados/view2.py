95b4f1b30fbd803c32a472b9cf7dd68c30036cf1
Carlos Rocha <crocha09.09@gmail.com>
2017-06-21 12:08:44 -0600

    agreando otra salida

--- resultados/view2.py
+++ resultados/view2.py
@@ -11,7 +11,7 @@ import collections
 from .views import _queryset_filtrado
 
 def grafo_comparativo(request, template='resultados/grafo-resultado.html'):
-    
+
     years = []
     for en in ResultadosEvaluacion.objects.order_by('year').values_list('year', flat=True):
         years.append((en,en))
@@ -30,11 +30,34 @@ def grafo_comparativo(request, template='resultados/grafo-resultado.html'):
         bloque7 = Mercados.objects.filter(resultado__year=anio[0],opciones=1).aggregate(average=Avg('valor'))['average']
         bloque8 = RiesgoExternos.objects.filter(resultado__year=anio[0],opciones=1).aggregate(average=Avg('valor'))['average']
         bloque9 = Facilitadores.objects.filter(resultado__year=anio[0],opciones=1).aggregate(average=Avg('valor'))['average']
-        
+
 
         comparativa_dict[anio[1]] = [bloque1,bloque2,bloque3,bloque4,bloque5,bloque6,
                                     bloque7,bloque8,bloque9]
 
-    print comparativa_dict
+    grafo_volumen = {}
+    for obj in CHOICE_TIPO_MERCADO:
+        valor = IncrementoAbastecimiento.objects.filter(tipo_mercado=obj[0]).count()
+        if valor > 0:
+            grafo_volumen[obj[1]] = valor
 
-    return render(request, template, {'comparativa':comparativa_dict})
+
+    years2 = []
+    for en in ResultadosImplementacion.objects.order_by('year').values_list('year', flat=True):
+        years2.append((en,en))
+
+    grafo_incremento = {}
+    for year in years2:
+        for obj in CHOICE_AUMENTADO_INGRESOS:
+            valor_hombre = AumentadoIngresos.objects.filter(resultado_implementacion__year=year[0],opcion=obj[0]).aggregate(hombre=Avg('cantidad_hombres'))['hombre']
+            valor_mujer = AumentadoIngresos.objects.filter(resultado_implementacion__year=year[0],opcion=obj[0]).aggregate(mujer=Avg('cantidad_mujeres'))['mujer']
+            suma_valores = valor_hombre + valor_mujer
+            if suma_valores > 0:
+                grafo_incremento[obj[1]] = suma_valores
+
+    print grafo_incremento
+
+    return render(request, template, {'comparativa':comparativa_dict,
+                                      'grafo_volumen':grafo_volumen,
+                                      'inicio_incremento':years2[0][0],
+                                      'grafo_incremento':grafo_incremento})
