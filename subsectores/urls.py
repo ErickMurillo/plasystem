9999997cbd26bcc7b6bc13b7259eef5426b0be19
Carlos Rocha <crocha09.09@gmail.com>
2017-09-05 17:00:06 -0600

    continuando el desarrollo de subsector

--- subsectores/urls.py
+++ subsectores/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+	url(r'^$', views.consulta_proyectos, name='index-subsectores'),
     url(r'^ver/informe-planes-anuales/$', views.list_informe, name='lista-informe'),
-    url(r'^ver/plan/(?P<pk>\d+)/$', views.ver_informe, name='ver-informe'),
+    url(r'^subsector/ver/proyecto/(?P<pk>\d+)/$', views.ver_informe, name='subsector-ver-proyecto'),
 
 ]
